237d969372fb519b7a4559fea7e1628be4199b81
Guru-Guna <guruguna.guru@gmail.com>
2021-11-27 15:53:59 +0530

    https://github.com/Guru-Guna/simplewebserver/commit/23ffc74a6a67d166d47ffa92f979fe14eca1cb8e

--- mywebserver.py
+++ mywebserver.py
@@ -6,7 +6,41 @@ content  = """
 <title>My webserver</title>
 </head>
 <body>
-<h1>Welcome</h1>
+<h1>top five programming languages</h1>
+
+<h1>1.javascript</h1>
+<p>JavaScript is the world's most popular programming language.
+
+JavaScript is the programming language of the Web.
+
+JavaScript is easy to learn.
+
+This tutorial will teach you JavaScript from basic to advanced.</p>
+
+<h1>2.c++</h1>
+<p>C++ is a cross-platform language that can be used to create high-performance applications.
+
+C++ was developed by Bjarne Stroustrup, as an extension to the C language.
+
+C++ gives programmers a high level of control over system resources and memory.
+
+The language was updated 3 major times in 2011, 2014, and 2017 to C++11, C++14, and C++17.</p>
+
+<h1>3.python</h1>
+<p>web development (server-side),
+software development,
+mathematics,
+system scripting.</p>
+
+<h1>4.kotllin</h1>
+<p>Kotlin is a general-purpose programming language originally developed and unveiled as Project Kotlin by JetBrains in 2011.
+The first version was officially released in 2016.
+It is interoperable with Java and supports functional programming languages.</p>
+
+<h1>5.swift</h1>
+<p>A few years ago, Swift made the top 10 in the monthly TIOBE Index ranking of popular programming languages.
+Apple developed Swift in 2014 for Linux and Mac applications.</p>
+
 </body>
 </html>
 """
