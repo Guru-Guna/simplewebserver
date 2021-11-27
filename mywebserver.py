23ffc74a6a67d166d47ffa92f979fe14eca1cb8e
Guru-Guna <guruguna.guru@gmail.com>
2021-11-27 15:16:15 +0530

    https://github.com/Guru-Guna/simplewebserver/commit/651a1b28777d32f1005b53586423e177ac51cec4

--- mywebserver.py
+++ mywebserver.py
@@ -1,26 +1,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
-content = """
+content  = """
+<!DOCTYPE html>
 <html>
 <head>
+<title>My webserver</title>
 </head>
 <body>
 <h1>Welcome</h1>
 </body>
 </html>
 """
-
-class HelloHandler(BaseHTTPRequestHandler):
+class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print("request received")
         self.send_response(200)
         self.send_header('Content-type', 'text/html;charset=utf-8')
         self.end_headers()
         self.wfile.write(content.encode())
-
-
-server_address = ('',80)
-httpd = HTTPServer(server_address,HelloHandler)
+server_address = ('',8080)
+httpd = HTTPServer(server_address,myHandler)
+print("my webserver is running...")
 httpd.serve_forever()
+
 
 
 
